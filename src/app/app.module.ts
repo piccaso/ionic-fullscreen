3be540fc7630c6ac45f209fc795a82b1b943fce9
Florian Fida <f+git@fida.biz>
2018-05-21 00:13:33 +0200

    use fullscreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { PowerManagement } from '@ionic-native/power-management';
+import { AndroidFullScreen } from '@ionic-native/android-full-screen';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -27,6 +28,7 @@ import { HomePage } from '../pages/home/home';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     PowerManagement,
+    AndroidFullScreen,
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PowerManagement } from '@ionic-native/power-management';
+import { AndroidFullScreen } from '@ionic-native/android-full-screen';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, private powerManagement: PowerManagement) {
+  constructor(
+    public navCtrl: NavController,
+    powerManagement: PowerManagement,
+    androidFullScreen: AndroidFullScreen
+  ){
     powerManagement.acquire()
       .then(()=>{
-        this.wakelock="available"
+        this.wakelock="available";
       })
       .catch(reason => {
         console.error("no-wakelock", reason);
         this.wakelock=`not available, reason=${reason}`;
-      });
+      })
+    ;
+    androidFullScreen.immersiveMode()
+      .then(()=>{
+        this.fullscreen = "available";
+      })
+      .catch(reason => {
+        console.error("no-fullscreen", reason);
+        this.fullscreen = `not available, reason=${reason}`;
+      })
+    ;
   }
 
   wakelock:string;
+  fullscreen:string;
 }
