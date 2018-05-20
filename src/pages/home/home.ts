220776a0d3576d72240d735bbb8b868a9aced8fd
Florian Fida <f+git@fida.biz>
2018-05-20 23:57:47 +0200

    use wakelock

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PowerManagement } from '@ionic-native/power-management';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private powerManagement: PowerManagement) {
+    powerManagement.acquire()
+      .then(()=>{
+        this.wakelock="available"
+      })
+      .catch(reason => {
+        console.error("no-wakelock", reason);
+        this.wakelock=`not available, reason=${reason}`;
+      });
   }
 
+  wakelock:string;
 }
