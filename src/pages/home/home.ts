691b4d48ead86cab1fb6d0116ed1c3cef2d893eb
Florian Fida <f+git@fida.biz>
2018-09-04 14:56:32 +0200

    status bar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PowerManagement } from '@ionic-native/power-management';
 import { AndroidFullScreen } from '@ionic-native/android-full-screen';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
 
 @Component({
   selector: 'page-home',
@@ -11,28 +13,48 @@ export class HomePage {
   constructor(
     public navCtrl: NavController,
     powerManagement: PowerManagement,
-    androidFullScreen: AndroidFullScreen
+    androidFullScreen: AndroidFullScreen,
+    private platform: Platform,
+    private statusbar: StatusBar,
   ) {
-    powerManagement.acquire()
-      .then(() => {
+    this.platform.ready().then(() => {
+
+      powerManagement.acquire().then(() => {
         this.wakelock = "available";
-      })
-      .catch(reason => {
+      }).catch(reason => {
         console.error("no-wakelock", reason);
-        this.wakelock = `not available, reason=${reason}`;
-      })
-      ;
-    androidFullScreen.immersiveMode()
-      .then(() => {
+        this.wakelock = `n/a, reason=${reason}`;
+      });
+
+      androidFullScreen.immersiveMode().then(() => {
         this.fullscreen = "available";
-      })
-      .catch(reason => {
+      }).catch(reason => {
         console.error("no-fullscreen", reason);
-        this.fullscreen = `not available, reason=${reason}`;
-      })
-      ;
+        this.fullscreen = `n/a, reason=${reason}`;
+      });
+
+      // androidFullScreen.showUnderSystemUI().then(() => {
+      //   this.underUi = "ok";
+      // }).catch(reason => {
+      //   this.underUi = `n/a, reason=${reason}`;
+      // });
+
+      this.statusbar.hide();
+      this.statusbar.backgroundColorByHexString('#ffffff');
+      this.platformState = "ready";
+
+      setInterval(() => {
+        if (this.statusbar.isVisible) {
+          this.statusbar.show();
+          this.statusbar.hide();
+        }
+      }, 1500);
+
+    })
   }
 
-  wakelock: string = "";
-  fullscreen: string = '';
+  underUi: string = '-';
+  wakelock: string = "-";
+  fullscreen: string = '-';
+  platformState: string = '-';
 }
